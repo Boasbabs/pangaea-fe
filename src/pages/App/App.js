3ef36aa1d3a680e94911dcb6a4c08ea7d2a6852f
Adeyemi Simeon <babalolasimeon@gmail.com>
2021-03-10 00:44:51 +0100

    refactor: move grapql query

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import {
   Grid,
-  Stack,
   Text,
   Heading,
   Select,
@@ -13,34 +12,11 @@ import {
   Flex,
   Spacer,
 } from '@chakra-ui/react';
-
-import styles from './app.module.scss';
+import { PRODUCT_QUERY, CURRENCY_QUERY} from "./graphql"
 
 import Logo from 'assets/images/lumin-logo.png';
+import styles from './app.module.scss';
 
-const PRODUCT_QUERY = gql`
-  query($currentCurrency: Currency!) {
-    products {
-      id
-      title
-      image_url
-      price(currency: $currentCurrency)
-      product_options {
-        title
-        options {
-          id
-          value
-        }
-      }
-    }
-  }
-`;
-
-const CURRENCY_QUERY = gql`
-  {
-    currency
-  }
-`;
 
 function App() {
   const currentCurrency = {
@@ -61,6 +37,8 @@ function App() {
     data: currencyData,
     error: currencyError,
   } = useQuery(CURRENCY_QUERY);
+
+  if (productError || currencyError) return <Box children="error" />
 
   return (
     <div className={styles.App}>
@@ -128,28 +106,6 @@ function App() {
           ))}
         </select>
       )}
-
-      {/* <main className="App-header">
-        <div>
-          {productLoading ? (
-            <h5> loading... </h5>
-          ) : (
-            productData?.products.map((product) => {
-              return (
-                <div key={product.id}>
-                  <div>{product.title}</div>
-                  <div>{product.price}</div>
-                  <img
-                    src={product.image_url}
-                    alt={`${product.title}-product`}
-                    srcSet=""
-                  />
-                </div>
-              );
-            })
-          )}
-        </div>
-      </main> */}
 
       <Skeleton height="50px" isLoaded={!productLoading}>
         <Grid templateColumns="repeat(3, 1fr)" gap={10}>
